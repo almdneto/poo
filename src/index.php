249f5160854eec76b72557fbc13eed5bcdf74f69
almdneto <airtinpvp13@gmail.com>
2026-03-10 17:02:08 -0300

    nao sei

--- src/index.php
+++ src/index.php
@@ -15,15 +15,15 @@
 
 
 </head>
-<body class='bg-purple-950 text-white flex'>
+<body class='bg-purple-950  text-white flex'>
     <form action="" method="get" class='border-4 border-purple-500 rounded-2xl flex justify-center flex-col items-center gap-4 m-4 p-4'>
-        <div >
-            <label for="n1" >1º Número</label>
-            <input type="number" name="n1" id="n1" class='border-b-2 border-purple-500  '>
+        <div class='relative '>
+            <label for="n1" class='absolute top-[0] pointer-events-none transition-all duration-500 peer-focus:-top-5'>1º Número</label>
+            <input type="number" name="n1" id="n1" class='peer border-b-2 w-full outline-none bg-transparent border-purple-500  '>
         </div>
-        <div>
-            <label for="n2">2º Número</label>
-            <input type="number" name="n2" id="n2" class='border-b-2 border-purple-500 '>
+        <div class='relative '>
+            <label for="n2" class='absolute top-[0] pointer-events-none transition-all duration-500 peer-focus:-top-5'>2º Número</label>
+            <input type="number" name="n2" id="n2" class='peer border-b-2 w-full outline-none bg-transparent border-purple-500 '>
         </div>
         <div>
             <label for="operacao">Operação</label>
@@ -45,21 +45,21 @@
 <?php
  $numero1 = floatval($_GET['n1'] ?? 0);
  $numero2 = floatval($_GET['n2'] ?? 0);
-$operacao = $_GET['operacao'] ?? '';
+ $operacao = $_GET['operacao'] ?? '';
 
-switch ($operacao){
-    case 'soma':
-        echo $calculadora->soma($numero1, $numero2);
-        break;
-    case 'subtracao':
-        echo $calculadora->subtracao($numero1, $numero2);
-        break;
-    case 'multiplicacao':
-        echo $calculadora->multiplicacao($numero1, $numero2);
-        break;
-    case 'divisao':
-        echo $calculadora->divisao($numero1, $numero2);
-        break;
-    default:
-        // echo "Operação inválida.";
-}
+    switch ($operacao){
+        case 'soma':
+            echo $calculadora->soma($numero1, $numero2);
+            break;
+        case 'subtracao':
+            echo $calculadora->subtracao($numero1, $numero2);
+            break;
+        case 'multiplicacao':
+            echo $calculadora->multiplicacao($numero1, $numero2);
+            break;
+        case 'divisao':
+            echo $calculadora->divisao($numero1, $numero2);
+            break;
+        default:
+            // echo "Operação inválida.";
+    }
